d29dbd0e925ff4d3414fa0913316509e2f0c442c
BitZ <85895666+iArtie@users.noreply.github.com>
2022-05-04 12:02:12 -0600

    Add files via upload

---
+++ Common/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class AppSettings
+    {
+        public static string ApiUrl { get => ConfigurationManager.AppSettings.Get("ApiUrl"); }
+        public static string ApiUrlGeo { get => ConfigurationManager.AppSettings.Get("ApiUrlGeo"); }
+        public static string Token { get => ConfigurationManager.AppSettings.Get("Token"); }
+        public static string units { get => ConfigurationManager.AppSettings.Get("Units"); }
+        public static string ApiIcon { get => ConfigurationManager.AppSettings.Get("ApiIcon"); }
+    }
+}
---
+++ Domain/Entities/WeatherMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class WeatherMain
+    {
+        public class weather
+        {
+            public int id { get; set; }
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+        public class current
+        {
+
+            public long dt { get; set; }
+            public int sunrise { get; set; }
+            public int sunset { get; set; }
+            public double temp { get; set; }
+            public double feels_like { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+            public double dew_point { get; set; }
+            public double uvi { get; set; }
+            public int clouds { get; set; }
+            public double wind_speed { get; set; }
+            public int wind_deg { get; set; }
+            public List<weather> weather { get; set; }
+        }
+        public class hourly
+        {
+            public long dt { get; set; }
+            public double temp { get; set; }
+            public double feels_like { get; set; }
+            public int pressure { get; set; }
+            public int humidity { get; set; }
+            public int clouds { get; set; }
+            public double wind_speed { get; set; }
+            public int wind_deg { get; set; }
+            public List<weather> weather { get; set; }
+        }
+        public class ForeCastInfo
+        {
+            public current current { get; set; }
+            public List<hourly> hourly { get; set; }
+
+        }
+    }
+   
+   
+}
