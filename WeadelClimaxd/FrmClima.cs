eb3f4bebad7200e592cf241b51ffae06be9d7426
BitZ <85895666+iArtie@users.noreply.github.com>
2022-05-04 12:01:11 -0600

    Add files via upload

---
+++ WeadelClimaxd/FrmClima.cs
@@ -0,0 +1,229 @@
+﻿using Domain.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Infraestructure.Transparencia;
+using Infraestructure.OpenWeatherClient;
+using Common;
+using System.IO;
+
+namespace WeadelClimaxd
+{
+    public partial class FrmClima : Form
+    {
+        public WeatherMain.ForeCastInfo wfc2;
+        public OpenWeatherClient opw;
+        public double x, y;
+        
+        string owajson;
+        long dt;
+        //long dt = DateTimeOffset.Now.ToUnixTimeSeconds();
+        public FrmClima()
+        {
+            opw = new OpenWeatherClient();
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            //BackColor = Color.Azure;
+            
+            //TransparencyKey = Color.Azure;
+            InitializeComponent();
+
+        }
+     
+       
+        string APIKey = "498e4fc3fddeb027b3363fcfc0b77921";
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+
+            flpContent.Hide();
+            wfc2 = null;
+            flpContent.Controls.Clear();
+            
+            try
+            {
+                getWeather();
+                flpContent.Show();
+                txtClima.Text = String.Empty;
+                string filename = "laweaclima.json";
+                //File.WriteAllText
+                //using (FileStream fs = File.Create(filename))
+                //{
+                //   fs.Write(owajson);
+                //    // Add some information to the file.
+                //    //fs.Write(info, 0, info.Length);
+                //}
+                //System.IO.FileStream Create(string filename);
+              
+            }
+            catch
+            {
+                flpContent.Show();
+                if (txtClima.Text == String.Empty)
+                {
+                    MessageBox.Show("Escriba una ciudad");
+                }else if (labelWn.Text == "N/A" )
+                {
+                    MessageBox.Show("No se pudo encontrar la ciudad");
+                    txtClima.Text = String.Empty;
+                }
+                
+                
+            }
+        }
+     
+
+        public class WindowExtension
+        {
+            [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+            private extern static void ReleaseCapture();
+          
+            
+
+       
+
+        }
+        public void getWeather()
+        {
+            try
+            {
+                using (WebClient web = new WebClient())
+                {
+
+                    string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", txtClima.Text, APIKey);
+                    var json = web.DownloadString(url);
+                   
+                    root Info = JsonConvert.DeserializeObject<root>(json);
+                    string sunset = ConvertDateTime(Info.sys.sunset).ToShortTimeString();
+                    string sunrise = ConvertDateTime(Info.sys.sunrise).ToShortTimeString();
+                    double temp = Info.main.temp;
+                    double unu = temp - 273.15;
+                    unu = (int)unu;
+                    picWeather.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                    labelCd.Text = Info.weather[0].main;
+                    laberDt.Text = Info.weather[0].description;
+                    labelSun.Text = sunset.ToString();
+                    labelSn.Text = sunrise.ToString();
+                    labelWn.Text = Info.wind.speed.ToString();
+                    labelPress.Text = Info.main.pressure.ToString();
+                    labelTemp.Text = unu.ToString() + "°";
+                    x = Info.coord.lat;
+                    y = Info.coord.lon;
+                    dt = Info.dt;
+                    owajson = JsonConvert.SerializeObject(Info);
+                    StreamWriter guardado = new StreamWriter();
+                    foreach (object linea in owajson)
+                    {
+                        guardado.WriteLine(linea);
+                    }
+                    guardado.Close();
+                }
+                
+                Task.Run(Request).Wait();
+                InfoDetail mini;
+                for (int i = 0; i < 9; i++)
+                {
+                    double temp;
+                    int tempint;
+                    string hoursstring;
+                    temp = wfc2.hourly[i].temp - 273.15;
+                    hoursstring = ConvertDateTime(wfc2.hourly[i].dt).ToShortTimeString();
+                    tempint = (int)temp;
+                    mini = new InfoDetail();
+                    mini.lblHours.Text = hoursstring;
+                    mini.lblCondi.Text = wfc2.hourly[i].weather[0].main;
+                    mini.lblDetails.Text = wfc2.hourly[i].weather[0].description;
+                    mini.lblWin.Text = wfc2.hourly[i].wind_speed.ToString();
+                    mini.lblPress.Text = wfc2.hourly[i].pressure.ToString();
+                    mini.lblTemp.Text = tempint.ToString() + "°";
+                    mini.picWeather.ImageLocation = $"{AppSettings.ApiIcon}" + wfc2.hourly[i].weather[0].icon + ".png";
+                    flpContent.Controls.Add(mini);
+                }
+                owajson = JsonConvert.SerializeObject(wfc2);
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("No se pudo encontrar la ciudad");
+            }
+        }
+
+       
+        
+        private void FrmClima_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void TxtClima_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)(Keys.Enter))
+            {
+                e.Handled = true;
+                BtnBuscar_Click(sender,e);
+            }
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("No puede agregar numeros");
+            }
+        }
+        DateTime ConvertDateTime(long Milisec)
+        {
+            DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
+
+            day = day.AddSeconds(Milisec).ToLocalTime();
+
+            return day;
+        }
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public async Task Request()
+        {
+            wfc2 = await opw.GetWeatherByGeo(x, y, dt);
+        }
+        private void FrmClima_Load(object sender, EventArgs e)
+        {
+
+            (new LayeredWindowHelper(this)).BackColor = Color.FromArgb(128, Win7Style.GetThemeColor());
+
+
+
+            Win7Style.EnableBlurBehindWindow(this.Handle);
+            Win10Style.EnableBlur(this.Handle);
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+          private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+                                      
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void TxtClima_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+      
+
+    }
+
+
---
+++ WeadelClimaxd/InfoDetail.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WeadelClimaxd
+{
+    partial class InfoDetail
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.lblCondi = new System.Windows.Forms.Label();
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.lblPress = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.picWeather = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picWeather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(142, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hours";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(131, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Condition";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(118, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Wind Speed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(142, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 24);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Details";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(131, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 24);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Pressure";
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHours.Location = new System.Drawing.Point(392, 11);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(61, 24);
+            this.lblHours.TabIndex = 17;
+            this.lblHours.Text = "Hours";
+            // 
+            // lblCondi
+            // 
+            this.lblCondi.AutoSize = true;
+            this.lblCondi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCondi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblCondi.Location = new System.Drawing.Point(392, 37);
+            this.lblCondi.Name = "lblCondi";
+            this.lblCondi.Size = new System.Drawing.Size(61, 24);
+            this.lblCondi.TabIndex = 18;
+            this.lblCondi.Text = "Hours";
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.AutoSize = true;
+            this.lblDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDetails.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDetails.Location = new System.Drawing.Point(392, 62);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(61, 24);
+            this.lblDetails.TabIndex = 19;
+            this.lblDetails.Text = "Hours";
+            // 
+            // lblWin
+            // 
+            this.lblWin.AutoSize = true;
+            this.lblWin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblWin.Location = new System.Drawing.Point(392, 87);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(61, 24);
+            this.lblWin.TabIndex = 20;
+            this.lblWin.Text = "Hours";
+            // 
+            // lblPress
+            // 
+            this.lblPress.AutoSize = true;
+            this.lblPress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPress.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPress.Location = new System.Drawing.Point(392, 111);
+            this.lblPress.Name = "lblPress";
+            this.lblPress.Size = new System.Drawing.Size(61, 24);
+            this.lblPress.TabIndex = 21;
+            this.lblPress.Text = "Hours";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemp.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTemp.Location = new System.Drawing.Point(26, 87);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(42, 39);
+            this.lblTemp.TabIndex = 22;
+            this.lblTemp.Text = "C";
+            // 
+            // picWeather
+            // 
+            this.picWeather.BackColor = System.Drawing.Color.Transparent;
+            this.picWeather.Location = new System.Drawing.Point(17, 11);
+            this.picWeather.Name = "picWeather";
+            this.picWeather.Size = new System.Drawing.Size(81, 59);
+            this.picWeather.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picWeather.TabIndex = 14;
+            this.picWeather.TabStop = false;
+            // 
+            // InfoDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(3)))), ((int)(((byte)(3)))));
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.lblPress);
+            this.Controls.Add(this.lblWin);
+            this.Controls.Add(this.lblDetails);
+            this.Controls.Add(this.lblCondi);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.picWeather);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "InfoDetail";
+            this.Size = new System.Drawing.Size(605, 164);
+            ((System.ComponentModel.ISupportInitialize)(this.picWeather)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.Label lblHours;
+        public System.Windows.Forms.Label lblCondi;
+        public System.Windows.Forms.Label lblDetails;
+        public System.Windows.Forms.Label lblWin;
+        public System.Windows.Forms.Label lblPress;
+        public System.Windows.Forms.Label lblTemp;
+        public System.Windows.Forms.PictureBox picWeather;
+    }
+}
